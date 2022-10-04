7b7e38c45682269a56c183ed845d8e7b4cd3d16e
francesco <frybene@libero.it>
2022-10-04 22:29:25 +0200

    Boh

--- clustering/spatial_clustering.py
+++ clustering/spatial_clustering.py
@@ -3,47 +3,28 @@ import numpy as np
 import math
 from sklearn_extra.cluster import KMedoids
 from sklearn.cluster import AgglomerativeClustering
-from sklearn.metrics import silhouette_score
 from pyclustering.cluster.kmedoids import kmedoids
 from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
 from pyclustering.cluster.elbow import elbow
 import matplotlib.pyplot as plt
-from utils import save_to_pickle
+from utils import save_to_pickle, load_from_pickle
 
-d = {}
-for row in arff.load('Fumagalli 8fold CV/train_2019.arff'):
-    index = row[0]
-    if index not in d.keys():
-        lat = row[1]
-        long = row[2]
-        d[index] = (lat, long)
+def create_distance_matrix(dst_path, src='Fumagalli 8fold CV/train_2019.arff'):
+    d = {}
+    for row in arff.load(src):
+        index = row[0]
+        if index not in d.keys():
+            lat = row[1]
+            long = row[2]
+            d[index] = (lat, long)
 
-keys_list = list(d.keys())
-distance_mat = np.zeros((len(d.keys()), len(d.keys())))
-for i in range(len(d.keys())):
-    for j in range(len(d.keys())):
-        if i != j:
-            distance_mat[i, j] = math.dist(d[keys_list[i]], d[keys_list[j]])
-
-n_clusters = 5
-inertias = []
-#clustering = KMedoids(n_clusters=n_clusters, random_state=0, metric='precomputed').fit(distance_mat)
-n_clusters = np.arange(3, 15)
-for n in n_clusters:
-    clustering = AgglomerativeClustering(n_clusters=n, affinity="precomputed", linkage="average")
-    #clustering = KMedoids(n_clusters=n, random_state=0, metric='precomputed')
-    clustering.fit(distance_mat)
-    lab = list(clustering.labels_)
-    if n == 8:
-        save_to_pickle("{}.pkl".format(n), clustering)
-        d = {l+1: lab.count(l) for l in set(lab)}
-        print(d)
-    inertias.append(silhouette_score(distance_mat, lab))
-
-
-#save_to_pickle("spatial_adj_matrix.pkl", distance_mat)
-#save_to_pickle("{}.pkl".format(n_clusters), clustering)
-#inertias.append(clustering.inertia_)
+    keys_list = list(d.keys())
+    distance_mat = np.zeros((len(d.keys()), len(d.keys())))
+    for i in range(len(d.keys())):
+        for j in range(len(d.keys())):
+            if i != j:
+                distance_mat[i, j] = math.dist(d[keys_list[i]], d[keys_list[j]])
+    save_to_pickle(dst_path, distance_mat)
 
 # Plot inertia for each k in order to find the best number of clusters with the elbow method
 def plot_inertia(n_clusters, inertias):
@@ -54,8 +35,8 @@ def plot_inertia(n_clusters, inertias):
     plt.ylabel("Silhouette")
     plt.xlabel("K")
     plt.show()
-plot_inertia(n_clusters, inertias)
-def pyclustering_clustering():
+
+'''def pyclustering_clustering():
     kmin, kmax = 1, 10
     elb = elbow(distance_mat, kmin, kmax)
     elb.process()
@@ -79,6 +60,18 @@ def pyclustering_clustering():
     ax.grid(True)
     plt.ylabel("WCE")
     plt.xlabel("K")
-    plt.show()
+    plt.show()'''
 
 
+def main(n_clusters, distance_mat_path, linkage="average"):
+    distance_mat = load_from_pickle(distance_mat_path)
+    clustering = AgglomerativeClustering(n_clusters=n_clusters, affinity="precomputed", linkage=linkage)
+    #clustering = KMedoids(n_clusters=n, random_state=0, metric='precomputed')
+    clustering.fit(distance_mat)
+    lab = list(clustering.labels_)
+    save_to_pickle("spatial_clustering/{}.pkl".format(n_clusters), clustering)
+    d = {l+1: lab.count(l) for l in set(lab)}
+    print(d)
+
+#n/2
+main(n_clusters=75, distance_mat_path="spatial_adj_matrix.pkl")
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import pickle
-from sklearn.preprocessing import MinMaxScaler
 
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 def save_to_pickle(name, c):
     with open(name, 'wb') as f:
         pickle.dump(c, f)
@@ -9,17 +11,54 @@ def load_from_pickle(name):
     with open(name, 'rb') as f:
         return pickle.load(f)
 
-def create_lstm_tensors(df, scaler, target_column):
+def absolute_scaler(x, max, testing=False):
+    if testing:
+        for i in range(len(x)):
+            if i <= max:
+                x[i] /= max
+            else:
+                x[i] = 1
+    else:
+        x = x/max
+    return x
+
+def create_lstm_tensors(df, target_column, max=False):
     months_columns = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']     # Names of the columns containing the training data
     x = df[months_columns].values
     y = df[target_column].values
+    testing = True
+    if not max:
+        max_x = np.max(x)
+        max_y = np.max(y)
+        if max_x > max_y:
+            max = max_x
+        else:
+            max = max_y
+        testing=False
 
-    x = scaler.fit_transform(x)
+    x = absolute_scaler(x, max, testing=testing)
+    y = absolute_scaler(y, max, testing=testing)
+    '''x = scaler.fit_transform(x)
+    
+    y = scaler.fit_transform(y)'''
     x = x.reshape(x.shape[0], x.shape[1], 1)
-
     y = y.reshape(y.shape[0], 1)
-    y = scaler.fit_transform(y)     # If we are testing we don't need to scale the y values
 
-    return x, y
+    return x, y, max
+
+def create_lstm_tensors_minmax(df, scaler):
+    data = df.values
+    if not scaler:
+        scaler = MinMaxScaler(feature_range=(0,1))
+        scaled = scaler.fit_transform(data)
+    else:
+        scaled = scaler.transform(data)
+
+    x, y = scaled[:, :-1], scaled[:, -1]
+    x = x.reshape(x.shape[0], x.shape[1], 1)
+    y = y.reshape(y.shape[0], 1)
+
+    return x, y, scaler
+
 
 
