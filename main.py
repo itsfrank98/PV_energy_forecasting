63229ab54581bae2538086d9ec35b9135915b0c2
francesco <frybene@libero.it>
2022-11-26 02:13:21 +0100

    main

---
+++ main.py
@@ -0,0 +1,64 @@
+import sys
+sys.path.append("model/prepare_data/")
+sys.path.append("model/")
+from clustering.merge_clusterings import main as merged_main
+from clustering.spatio_temporal_clustering import main as spatio_temporal_main
+from model.aggregated_model import main as aggregated_model_main
+from model.prepare_data.aggregated import main as aggregated_main
+import argparse
+
+if __name__ == "__main__":
+    s = input("S: ")
+    t = input("T: ")
+    technique = input("Technique: ")
+    dataset = "pvitaly"
+    dst = "clustering/merged_clustering/{}/matrices".format(dataset)
+    train_data_path = "{}/single_target/train".format(dataset)
+    test_data_path = "{}/single_target/test".format(dataset)
+
+    merged_parse = argparse.Namespace
+    merged_parse.spatial_path = "{}/clustering/spatial_clustering/{}.pkl".format(dataset, s)
+    merged_parse.temporal_path = "{}/clustering/temporal_clustering/{}.pkl".format(dataset, t)
+    merged_parse.dst = dst
+    merged_parse.spatial_dist_mat = "{}/clustering/spatial_clustering/distance_mat.pkl".format(dataset)
+    merged_parse.temporal_dist_mat = "{}/clustering/temporal_clustering/distance_matrix.pkl".format(dataset)
+    merged_parse.technique = technique
+    #merged_main(merged_parse)
+
+    n = int(input("N clusters: "))
+    spatio_temp_parse = argparse.Namespace
+    spatio_temp_parse.n_clusters = n
+    spatio_temp_parse.distance_mat_path = "{}/{}_{}s{}t.pkl".format(dst, technique, s, t)
+    path_to_dictionary = "clustering/merged_clustering/{}/{}/{}s{}t_dict_{}.pkl".format(dataset, technique, s, t, n)
+    spatio_temp_parse.clusters_dict_path = path_to_dictionary
+    spatio_temp_parse.clustering_model_path = "clustering/merged_clustering/{}/{}/{}s{}t_clustering_{}.pkl".format(dataset, technique, s, t, n)
+    spatio_temp_parse.dataset = dataset
+    spatio_temporal_main(spatio_temp_parse)
+
+    aggregated_parse = argparse.Namespace
+    aggregated_parse.train_data_path = train_data_path
+    aggregated_parse.testing_data_path = test_data_path
+    aggregated_parse.s = "merged"
+    aggregated_parse.path_to_dictionary = path_to_dictionary
+    aggregated_parse.number_of_columns = 18
+    aggregated_parse.dst_folder = "model/{}_merged_{}/".format(dataset, technique)
+    aggregated_parse.y_cols = ['17']
+    aggregated_main(aggregated_parse)
+
+    model_parse = argparse.Namespace
+    c = path_to_dictionary.split("/")[-1].split("_")[0]
+    model_parse.train_dir = "model/{}_merged_{}/{}/aggregated_{}/train".format(dataset, technique, n, c)
+    model_parse.test_dir = "model/{}_merged_{}/{}/aggregated_{}/test".format(dataset, technique, n, c)
+    model_parse.file_name = "model/{}_merged_{}/{}/aggregated_{}/results.txt".format(dataset, technique, n, c)
+    model_parse.model_folder = "model/{}_merged_{}/{}/aggregated_{}/models".format(dataset, technique, n, c)
+    model_parse.neurons = 18
+    model_parse.dropout = 0.3
+    model_parse.lr = 0.008
+    model_parse.batch_size = 200
+    model_parse.epochs = 200
+    model_parse.patience = 10
+    model_parse.preprocess = 0
+    model_parse.clustering_dict_path = path_to_dictionary
+    model_parse.y_column = 1
+    aggregated_model_main(model_parse)
+
