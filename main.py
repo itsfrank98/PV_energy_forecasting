b494a39d33668eb862663fa84aa17b7d344393ed
francesco <frybene@libero.it>
2022-11-26 23:57:11 +0100

    Script for launching a lot of tests automatically

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ sys.path.append("model/")
 from clustering.merge_clusterings import main as merged_main
 from clustering.spatio_temporal_clustering import main as spatio_temporal_main
 from model.aggregated_model import main as aggregated_model_main
-from model.prepare_data.aggregated import main as aggregated_main
+from model.run_models import main as single_target_clustering_model_main
+from model.prepare_data.aggregate import main as aggregated_main
+from model.prepare_data.multitarget_prepare import main as prepare_single_target_clustering_main
 import argparse
 
 dataset = "pvitaly"
@@ -12,66 +14,77 @@ dst = "clustering/merged_clustering/{}/matrices".format(dataset)
 train_data_path = "{}/single_target/train".format(dataset)
 test_data_path = "{}/single_target/test".format(dataset)
 def merge_clusters(s, t, technique):
-    merged_parse = argparse.Namespace
-    merged_parse.spatial_path = "{}/clustering/spatial_clustering/{}.pkl".format(dataset, s)
-    merged_parse.temporal_path = "{}/clustering/temporal_clustering/{}.pkl".format(dataset, t)
-    merged_parse.dst = dst
-    merged_parse.spatial_dist_mat = "{}/clustering/spatial_clustering/distance_mat.pkl".format(dataset)
-    merged_parse.temporal_dist_mat = "{}/clustering/temporal_clustering/distance_matrix.pkl".format(dataset)
-    merged_parse.technique = technique
-    merged_main(merged_parse)
+    parser = argparse.Namespace
+    parser.spatial_path = "{}/clustering/spatial_clustering/{}.pkl".format(dataset, s)
+    parser.temporal_path = "{}/clustering/temporal_clustering/{}.pkl".format(dataset, t)
+    parser.dst = dst
+    parser.spatial_dist_mat = "{}/clustering/spatial_clustering/distance_mat.pkl".format(dataset)
+    parser.temporal_dist_mat = "{}/clustering/temporal_clustering/distance_matrix.pkl".format(dataset)
+    parser.technique = technique
+    merged_main(parser)
 
-def spatio_temporal_clustering(s, t, technique, n):
-    spatio_temp_parse = argparse.Namespace
-    spatio_temp_parse.n_clusters = n
-    spatio_temp_parse.distance_mat_path = "{}/{}_{}s{}t.pkl".format(dst, technique, s, t)
-    path_to_dictionary = "clustering/merged_clustering/{}/{}/{}s{}t_dict_{}.pkl".format(dataset, technique, s, t, n)
-    spatio_temp_parse.clusters_dict_path = path_to_dictionary
-    spatio_temp_parse.clustering_model_path = "clustering/merged_clustering/{}/{}/{}s{}t_clustering_{}.pkl".format(dataset, technique, s, t, n)
-    spatio_temp_parse.dataset = dataset
-    spatio_temporal_main(spatio_temp_parse)
-    return path_to_dictionary
+def spatio_temporal_clustering(s, t, technique, n, path_to_dictionary):
+    parser = argparse.Namespace
+    parser.n_clusters = n
+    parser.distance_mat_path = "{}/{}_{}s{}t.pkl".format(dst, technique, s, t)
+    parser.clusters_dict_path = path_to_dictionary
+    parser.clustering_model_path = "clustering/merged_clustering/{}/{}/{}s{}t_clustering_{}.pkl".format(dataset, technique, s, t, n)
+    parser.dataset = dataset
+    spatio_temporal_main(parser)
 
-def aggregate(path_to_dictionary, technique):
-    aggregated_parse = argparse.Namespace
-    aggregated_parse.train_data_path = train_data_path
-    aggregated_parse.testing_data_path = test_data_path
-    aggregated_parse.s = "merged"
-    aggregated_parse.path_to_dictionary = path_to_dictionary
-    aggregated_parse.number_of_columns = 18
-    aggregated_parse.dst_folder = "model/{}_merged_{}/".format(dataset, technique)
-    aggregated_parse.y_cols = ['17']
-    aggregated_main(aggregated_parse)
+def prepare_data(path_to_dictionary, technique, aggregate, model_type):
+    parser = argparse.Namespace
+    parser.train_data_path = train_data_path
+    parser.testing_data_path = test_data_path
+    parser.path_to_dictionary = path_to_dictionary
+    parser.dst_folder = "model/{}_merged_{}".format(dataset, technique)
+    parser.number_of_columns = 18
+    if aggregate:
+        parser.s = "merged"
+        parser.y_cols = ['17']
+        aggregated_main(parser)
+    else:
+        parser.axis = 0
+        parser.model_type = model_type
+        prepare_single_target_clustering_main(parser)
 
-def single_target_clustering():
-
-
-def train_test_model(path_to_dictionary, technique, n):
+def train_test_model(path_to_dictionary, technique, n, model_type):
     c = path_to_dictionary.split("/")[-1].split("_")[0]
-    model_parse = argparse.Namespace
-    model_parse.train_dir = "model/{}_merged_{}/{}/aggregated_{}/train".format(dataset, technique, n, c)
-    model_parse.test_dir = "model/{}_merged_{}/{}/aggregated_{}/test".format(dataset, technique, n, c)
-    model_parse.file_name = "model/{}_merged_{}/{}/aggregated_{}/results.txt".format(dataset, technique, n, c)
-    model_parse.model_folder = "model/{}_merged_{}/{}/aggregated_{}/models".format(dataset, technique, n, c)
-    model_parse.neurons = 18
-    model_parse.dropout = 0.3
-    model_parse.lr = 0.008
-    model_parse.batch_size = 200
-    model_parse.epochs = 200
-    model_parse.patience = 10
-    model_parse.preprocess = 0
-    model_parse.clustering_dict_path = path_to_dictionary
-    model_parse.y_column = 1
-    aggregated_model_main(model_parse)
+    parser = argparse.Namespace
+    parser.train_dir = "model/{}_merged_{}/{}/{}_{}/train".format(dataset, technique, n, model_type, c)
+    parser.test_dir = "model/{}_merged_{}/{}/{}_{}/test".format(dataset, technique, n, model_type, c)
+    parser.file_name = "model/{}_merged_{}/{}/{}_{}/results.txt".format(dataset, technique, n, model_type, c)
+    parser.model_folder = "model/{}_merged_{}/{}/{}_{}/models".format(dataset, technique, n, model_type, c)
+    parser.neurons = 18
+    parser.dropout = 0.3
+    parser.lr = 0.008
+    parser.batch_size = 200
+    parser.epochs = 1
+    parser.patience = 10
+    parser.preprocess = 0
+    parser.clustering_dict_path = path_to_dictionary
+    parser.y_column = -1
+    if model_type == "aggregated":
+        aggregated_model_main(parser)
+    elif model_type == "single_model_clustering":
+        parser.training_type = model_type
+        single_target_clustering_model_main(parser)
+
 
 
 if __name__ == "__main__":
+    # for model_type in ['aggregated', 'single_model_clustering']:
+    model_type = 'single_model_clustering'
     for technique in ["modularity", "consensus", "silhouette", "mi"]:
         for tup in [(4, 5), (5, 4), (4, 4), (5, 5)]:
             s = tup[0]
             t = tup[1]
-            merge_clusters(s, t, technique)
+            #merge_clusters(s, t, technique)
             for n in [4,5,6]:
-                path_to_dictionary = spatio_temporal_clustering(s, t, technique, n)
-                aggregate(path_to_dictionary, technique)
-                train_test_model(path_to_dictionary, technique, n)
+                path_to_dictionary = "clustering/merged_clustering/{}/{}/{}s{}t_dict_{}.pkl".format(dataset, technique, s, t, n)
+                #spatio_temporal_clustering(s=s, t=t, technique=technique, n=n, path_to_dictionary=path_to_dictionary)
+                aggregate = False
+                if model_type == 'aggregated':
+                    aggregate = True
+                prepare_data(path_to_dictionary=path_to_dictionary, technique=technique, aggregate=aggregate, model_type=model_type)
+                train_test_model(path_to_dictionary=path_to_dictionary, technique=technique, n=n, model_type=model_type)
